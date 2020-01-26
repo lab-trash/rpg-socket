0b9281db19dc0ed9a5f0f2792487ef5d86f95a48
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2020-01-26 04:17:32 +0100

    trying to fix cors

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 
 const app = require('express')();
-const cors = require('cors');
 const https = require('https');
 const querystring = require('querystring');
 
 const server = require('http').createServer(app);
 
-app.use(cors((req) => {
-  return {
-    origin: req.headers.origin
-  }
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 const io = require('socket.io')(server, {
   serveClient: false,
